103d1da16e94c0a2dc3d6cc5487e5ac719cdec9b
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-02-10 16:35:34 +0500

    just pratic of the python is here

---
+++ 3rd.py
@@ -0,0 +1,7 @@
+import os
+# select the direction which you want
+path = "."  #
+# using the os module to list the direction
+content = os.listdir(path)
+# print the content of the direction
+print(content)
